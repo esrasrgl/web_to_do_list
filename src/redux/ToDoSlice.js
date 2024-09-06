7b3f38b18f376671316916b173fdb72f80dfc783
Esra Sarıgül <esra.sarigul14@gmail.com>
2024-09-06 22:19:32 +0300

    redux store updated

--- src/redux/ToDoSlice.js
+++ src/redux/ToDoSlice.js
@@ -1,44 +1,34 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 const initialState = {
-  toDoItems: [
-    { text: "task1", id: 1, isDone: false },
-    { text: "task2", id: 2, isDone: false },
-  ],
+  toDoItems: [],
 };
+export const getToDoInfo = createAsyncThunk("getToDoInfo", async (userId) => {
+  const response = await axios.get(
+    `https://jsonplaceholder.typicode.com/todos?userId=${userId}`
+  );
+  const data = await response.data;
+  return data;
+});
 
 export const ToDoSlice = createSlice({
   name: "toDO",
   initialState: initialState,
-  reducers: {
-    add_item: (state, action) => {
-      const text = action.payload;
-      const id = Math.random().toString();
-      const isDone = false;
-      console.log(text, id, isDone);
-      state.toDoItems.push({ text, id, isDone });
-    },
-    delete_item(state, action) {
-      const id = action.payload;
-      state.toDoItems = state.toDoItems.filter((toDo) => toDo.id !== id);
-    },
-    edit_item(state, action) {
-      const { id, newText } = action.payload;
-      const item = state.toDoItems.find((item) => item.id === id);
-      if (item) {
-        item.text = newText;
-      }
-    },
-    is_done_item(state, action) {
-      const { id, checked } = action.payload;
-      const item = state.toDoItems.find((i) => i.id == id);
-      if (item) {
-        item.isDone = checked;
-      }
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getToDoInfo.fulfilled, (state = initialState, action) => {
+        state.toDoItems = action.payload;
+      })
+      .addCase(getToDoInfo.pending, (state = initialState) => {
+        // loading or animation
+        state.toDoItems = {};
+      })
+      .addCase(getToDoInfo.rejected, (state = initialState) => {
+        // error
+        state.toDoItems = {};
+      });
   },
 });
 
 export default ToDoSlice.reducer;
-export const { add_item, delete_item, edit_item, is_done_item } =
-  ToDoSlice.actions;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ToDoSlice from "./ToDoSlice";
+import UserSlice from "./UserSlice";
 
 export default function createStore(preloadedState) {
   return configureStore({
     reducer: {
       toDo: ToDoSlice,
+      users: UserSlice,
     },
     preloadedState,
   });
