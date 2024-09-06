5ee0fb798a93ea989f297704e0ec8de4050360e1
Esra Sarıgül <esra.sarigul14@gmail.com>
2024-09-06 22:40:51 +0300

    components updated created users component

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,60 +1,11 @@
-import { useState } from "react";
-import { delete_item, edit_item, is_done_item } from "../redux/ToDoSlice";
-import { useDispatch } from "react-redux";
-
 function ToDoItem({ task }) {
-  const [editText, SeteditText] = useState(task.text);
-  const [editIndex, SeteditIndex] = useState(null);
-  const dispatch = useDispatch();
   const index = task.id;
-
-  function checkBtn(index) {
-    if (editText.trim() === "") {
-      alert("Warning Invalid input");
-      SeteditText(task.text);
-    } else {
-      dispatch(edit_item({ id: index, newText: editText }));
-      SeteditIndex(null);
-    }
-  }
-
+  console.log("task ", task);
   return (
     <li key={index}>
-      {index !== editIndex ? (
-        <>
-          <input
-            type="checkBox"
-            onChange={(val) =>
-              dispatch(is_done_item({ id: index, checked: val.target.checked }))
-            }
-          />
-          <span className={`text ${task.isDone ? "strikethrough" : ""}`}>
-            {task.text}
-          </span>
-          <button className="editBtn" onClick={() => SeteditIndex(index)}>
-            Edit
-          </button>
-          <button
-            className="deleteBtn"
-            onClick={() => dispatch(delete_item(index))}
-          >
-            Delete
-          </button>
-        </>
-      ) : (
-        <>
-          <input
-            type="text"
-            className="editText"
-            placeholder="edit task"
-            value={editText}
-            onChange={(e) => SeteditText(e.target.value)}
-          ></input>
-          <button className="checkBtn" onClick={() => checkBtn(index)}>
-            Check
-          </button>
-        </>
-      )}
+      <span className={`text ${task.completed ? "strikethrough" : ""}`}>
+        {task.title}
+      </span>
     </li>
   );
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ToDoItem from "./ToDoItem";
 import { useSelector } from "react-redux";
-
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getToDoInfo } from "../redux/ToDoSlice";
+import { Link } from "react-router-dom";
 const ToDOList = () => {
   const toDoItems = useSelector((state) => state.toDo.toDoItems);
+  const {userId} = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getToDoInfo(userId));
+  }, [dispatch, userId]);
+
+  console.log(Array.isArray(toDoItems)); // true olmalı
+  console.log("toDoItems: ", toDoItems);
+  console.log("toDoItems: id ",userId);
 
   return (
     <div className="toDoList">
       <div className="listDiv">
         <ol>
-          {toDoItems.map((task, index) => (
-            <ToDoItem key={task.id} task={task} />
-          ))}
+          {(toDoItems && Array.isArray(toDoItems) ? toDoItems : []).map(
+            (task, index) => (
+              <ToDoItem key={task.id} task={task} />
+            )
+          )}
         </ol>
-      </div>
+        <Link to="/users">Go to Users</Link>
+        </div>
     </div>
   );
 };
