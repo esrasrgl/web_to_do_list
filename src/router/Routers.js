cf7df22c0ec3a4ccfb09b2e76e61633aa4b0654b
Esra Sarıgül <esra.sarigul14@gmail.com>
2024-09-09 00:57:58 +0300

    router updated

--- src/router/Routers.js
+++ src/router/Routers.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ToDOList from "../components/ToDoList";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Users from "../components/Users";
+import Users from "../components/Users/Users";
 import { Link } from "react-router-dom";
 export default function Routers() {
   return (
